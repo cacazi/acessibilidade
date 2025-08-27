3f9a0276cc852ef675afdc508fed20e66016e494
cacazi <00001094842643sp@al.educacao.sp.gov.br>
2025-08-27 08:20:38 -0300

    Add files via upload

---
+++ ACESSIBILIDADE caique 3c/script.js
@@ -0,0 +1,10 @@
+// script.js
+function aumentarFonte() {
+  document.body.style.fontSize = "larger";
+}
+function diminuirFonte() {
+  document.body.style.fontSize = "smaller";
+}
+function alternarContraste() {
+  document.body.classList.toggle("contraste");
+}
